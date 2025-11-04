f4005efa9b2f7fbde21e31689cd405631e3ea732
seif <SE2300587@tkh.edu.eg>
2025-11-04 12:35:17 +0200

    s

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
-const TripRouter =require('./routes/TripRouter');//
-const authRouter = require('./routes/authRouter');
+const TripRouter =require('./routes/TripRouter.js');//
+const authRouter = require('./routes/authRouter.js');
 
 dotenv.config();
 app.use(cors());
 
 app.use(express.json());
-app.use('/api/v1/trips', TripRouter);
+app.use('/trips', TripRouter);
 app.use('/auth', authRouter); // was missing / before auth like this: app.use('auth', AuthRouter); 
 // app.use('/api/v1/user', UserRouter);
 
-module.exports= {app}
+module.exports= {
+    app,
+
+
+}
 
